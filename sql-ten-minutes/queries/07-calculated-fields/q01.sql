84b75afa0f1705de15515c6725afd7f98a88c575
Remi de Fleurian <r.defleurian@qmul.ac.uk>
2021-08-05 22:25:53 -0400

    Change tabs to spaces

--- sql-ten-minutes/queries/07-calculated-fields/q01.sql
+++ sql-ten-minutes/queries/07-calculated-fields/q01.sql
@@ -1,6 +1,6 @@
 select vend_id, 
        vend_name as vname, 
-	   vend_address as vaddress, 
-	   vend_city as vcity
+       vend_address as vaddress, 
+       vend_city as vcity
   from Vendors
  order by vname;
--- sql-ten-minutes/queries/11-subqueries/q03.sql
+++ sql-ten-minutes/queries/11-subqueries/q03.sql
@@ -1,10 +1,10 @@
 select cust_email
   from Customers
  where cust_id in
-	   (select distinct cust_id
-	   from Orders
-	   where order_num in
-			 (select distinct order_num
-			 from OrderItems
-			 where prod_id = 'BR01'))
+       (select distinct cust_id
+       from Orders
+       where order_num in
+             (select distinct order_num
+             from OrderItems
+             where prod_id = 'BR01'))
  order by cust_email;
